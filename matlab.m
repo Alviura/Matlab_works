104790ac80b7d19ad0137b1340712b43d21fc023
Alviura <alviura@outlook.com>
2024-11-28 10:10:22 +0300

    Update Lesson 1

--- matlab.m
+++ matlab.m
@@ -35,3 +35,51 @@ m_inv=inv(p); % Evaluate the inverse of a matrix
 m_test=p*m_inv; 
 m_trans=p';
 
+a=3.6
+b=3.2
+c=-7.2
+d=-7.6
+% The ceil function - round off to the positive side
+fprintf('The cell function')
+h=ceil(a)
+j=ceil(b)
+k=ceil(c)
+l=ceil(d)
+
+%The floor function - round oof to the negative side
+fprintf("The floor function")
+f=floor(a)
+i=floor(b)
+m=floor(c);
+n=floor(d)
+
+% The round function
+fprintf("The round off function")
+v=round(a)
+u=round(b)
+s=round(c)
+n=round(d)
+
+% the abs function
+fprintf("the abs function")
+q=abs(a);
+x=abs(b)
+p=eq(e,f) % Checks if e is equal to f and returns a bool integer depending of result
+t=gt(e,f) % Checks if e is greater than f
+r=ge(e,f) % Checks if e is greater or equal to f
+le(e,f) % Checks if e is less than of=r equal to f
+
+% Set Operations
+d = [2,3,4,5,34,23,67,13]
+i = [2,3,4,5,6,7,8,122,334]
+
+union (d,i) % Find the union between two sets
+
+intersection(d,i) % set intersection
+
+setdiff(d,i) % set difference
+setdiff(i,d)
+
+setxor(d,i) % Set exclusive or 
+
+
