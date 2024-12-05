074c5fb801d9933222fa0142b151747ad941042b
Alviura <alviura@outlook.com>
2024-12-05 20:44:17 +0300

    Lesson 2

---
+++ grading.m
@@ -0,0 +1,11 @@
+x=input("Enter x");
+if x>70
+    fprintf("A");
+elseif x>=60 && x<70
+    fprintf("B")
+elseif x>=50 && x<60
+    fprintf("C")
+elseif x>=40 && x<50
+    fprintf("D")
+else x<40
+    fprintf("Fail")
