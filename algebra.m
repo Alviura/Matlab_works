af33e34eb911048d2f8229d02e28826683138bcd
Unknown <alvin@archlinux.localdomain>
2024-12-09 21:39:06 +0300

    Algebraic equation; solve function

---
+++ algebra.m
@@ -0,0 +1,6 @@
+% Algebra
+%The solve function can also solve high order ewuations. Often used to solve quadratic equations.
+%It can also be used to generate solutions of systems of equations
+%involving more than one variable.
+eq = 'x - 3 == 0';
+solve(eq,'x') 
