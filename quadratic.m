20d2217f2f56282a9f710f1431a550097416edd0
Alviura <alviura@outlook.com>
2024-12-12 22:56:49 +0300

    Update on discriminant

--- quadratic.m
+++ quadratic.m
@@ -2,19 +2,27 @@
 % enter three variables a, b & c
 
 a = input("Enter a:");
-b = input("Enter b:");
-c = input("Enter c:");
+b = input("Enter b: ");
+c = input("ENter c:");
 
-disc = b^2 - 4*a*c;
-x1 = (-b+sqrt(disc))/2*a;
-x2 = (-b-sqrt(disc))/2*a;
-if disc > 0
-    fprintf("The roots are distinct and real");
-elseif disp < 0
-    fprintf("The roots are distinct are equal");
+d = b^2 + 4*a*c;
+if d > 0
+    fprintf('There are two distict roots');
+    x1 = (-b + sqrt(d))/2*a;
+    x2 = (-b  sqrt(d))/2*a;
+    fprintf("first root is: %f", x1);
+    fprintf("second root is: %f", x2);
+
+elseif d = 0
+    fprintf('the roots are equal');
+    x1 = (-b + sqrt(d))/2*a;
+    x2 = (-b  sqrt(d))/2*a;
+    fprintf("first root is: %f", x1);
+    fprintf("second root is: %f", x2);
+
+else 
+    fprintf('the roots are complex')
+
 end
-    fprintf("The roots are complex %d\n", x1);
 
-fprintf("The first root is %d/n", x1);
-fprint("The second root is %d", x2)
 
