b6362a58d31e3de5b81bcd17db401051d993d122
Alviura <alviura@outlook.com>
2024-12-12 22:56:31 +0300

    Grading program

--- grading.m
+++ grading.m
@@ -1,11 +1,18 @@
-x=input("Enter x");
-if x>70
-    fprintf("A");
-elseif x>=60 && x<70
-    fprintf("B")
-elseif x>=50 && x<60
-    fprintf("C")
-elseif x>=40 && x<50
-    fprintf("D")
-else x<40
-    fprintf("Fail")
+score = input('Input score:');
+grade = '';
+switch (score)
+case score >=90
+    grade = 'A';
+case score >= 80
+    grade ='B';
+case score >=70
+    grade = 'C';
+case score >=60
+    grade = 'C'
+case score >=50
+    grade = 'D'
+case score >=40
+    grade = 'E'
+otherwise
+    grade = 'F'
+fprintf('The grade is %s', grade);
